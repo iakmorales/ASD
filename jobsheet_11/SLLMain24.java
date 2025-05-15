17a416394cfab9ed29a120d37873d0a222dc2793
iakmorales <zaskiamaulidina12@gmail.com>
2025-05-15 12:35:24 +0700

    jb 11 p1

--- jobsheet_11/SLLMain24.java
+++ jobsheet_11/SLLMain24.java
@@ -1,21 +1,69 @@
 package jobsheet_11;
-
+import java.util.Scanner;
 public class SLLMain24 {
 
     public static void main(String[] args) {
-        SingleLinkedList24 sll = new SingleLinkedList24();
-        Mahasiswa24 mhs1 = new Mahasiswa24("24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa24 mhs2 = new Mahasiswa24("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa24 mhs3 = new Mahasiswa24("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa24 mhs4 = new Mahasiswa24("21212203", "Dirga", "4D", 3.6);
+        Scanner input24 = new Scanner(System.in);
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertArt(2, mhs2);
-        sll.print();  
+        SingleLinkedList24 sll = new SingleLinkedList24();
+        int pilihan;
+        do {
+            System.out.println("==== Program Single Linked List ====");
+            System.out.println("1. Tambah Data Mahasiswa di awal");
+            System.out.println("2. Tambah Data Mahasiswa di akhir");
+            System.out.println("3. Tambah Data Mahasiswa setelah data tertentu");
+            System.out.println("4. Tambah Data Mahasiswa di posisi tertentu");
+            System.out.println("5. Tampilkan Data Mahasiswa");
+            System.out.println("0. Keluar");
+            System.out.print("Masukkan pilihan (0-5): ");
+            pilihan = input24.nextInt();
+            input24.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.println("=== Tambah Data Mahasiswa di Awal ===");
+                    sll.addFirst(inputMhs(input24));
+                    break;
+                case 2:
+                    System.out.println("=== Tambah Data Mahasiswa di Akhir ===");
+                    sll.addLast(inputMhs(input24));
+                    break;
+                case 3:
+                    System.out.println("=== Tambah Data Mahasiswa Setelah Data Tertentu ===");
+                    System.out.print("Masukkan nama mahasiswa yang diinginkan: ");
+                    String key = input24.nextLine();
+                    sll.insertAfter(key, inputMhs(input24));
+                    break;
+                case 4:
+                    System.out.println("=== Tambah Data Mahasiswa di Posisi Tertentu ===");
+                    System.out.print("Masukkan posisi yang diinginkan: ");
+                    int pos = input24.nextInt();
+                    input24.nextLine();
+                    sll.insertArt(pos, inputMhs(input24));
+                    break;
+                case 5:
+                    System.out.println("=== Tampilkan Data Mahasiswa ===");
+                    sll.print();
+                    break;
+                case 0:
+                    System.out.println("Terima kasih!");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan coba lagi.");
+            }
+        } while (pilihan != 0);
+    }
+
+    static Mahasiswa24 inputMhs(Scanner input24) {
+        System.out.print("Masukkan NIM: ");
+        String nim = input24.nextLine();
+        System.out.print("Masukkan Nama: ");
+        String nama = input24.nextLine();
+        System.out.print("Masukkan Kelas: ");
+        String kelas = input24.nextLine();
+        System.out.print("Masukkan IPK: ");
+        double ipk = input24.nextDouble();
+        input24.nextLine();
+        return new Mahasiswa24(nim, nama, kelas, ipk);
     }
 }
