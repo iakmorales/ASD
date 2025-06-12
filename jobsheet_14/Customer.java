14b405d74a4cb2c041f4b33489349723b312a9f1
iakmorales <zaskiamaulidina12@gmail.com>
2025-06-12 14:58:28 +0700

    jobsheet 14

---
+++ jobsheet_14/Customer.java
@@ -0,0 +1,19 @@
+package jobsheet_14;
+
+public class Customer {
+    public int id;
+    public String name;
+
+    public Customer() {
+
+    }
+
+    public Customer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return " ID: " + this.id + " Name: " + this.name;
+    }
+}
